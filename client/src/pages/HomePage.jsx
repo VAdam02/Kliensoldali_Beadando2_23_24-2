76f4d3e48ee958428262c7f07e2c32aa8756014c
VAdam02 <adam.vilanyi2@gmail.com>
2024-06-02 19:16:50 +0200

    Filter

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
 import * as React from "react"
 import { useDispatch, useSelector } from "react-redux";
-import { useGetJobsQuery } from "../store/reducers/jobApiSlice";
+import { useGetJobsWithFilterQuery } from "@/store/reducers/jobApiSlice";
 import { setJobs } from "@/store/reducers/jobSlice";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch"
 
 const HomePage = () => {
-    const { data: jobs, error, isLoading } = useGetJobsQuery();
+    const [ salaryFrom, setSalaryFrom ] = React.useState(0);
+    const [ type, setType ] = React.useState("any");
+    const [ city, setCity ] = React.useState(undefined);
+    const [ homeOffice, setHomeOffice ] = React.useState(undefined);
+    const { data: jobs, error, isLoading } = useGetJobsWithFilterQuery({ salaryFrom, type, city, homeOffice});
+
     const dispatch = useDispatch();
     const jobsState = useSelector(state => state.job.jobs);
     const jobCount = useSelector(state => state.job.total);
@@ -18,8 +26,44 @@ const HomePage = () => {
     return (
         <div>
             <h1>Home Page</h1>//TODO
+            <div>
+                <Input 
+                    placeholder="Minimum fizetés" 
+                    type="number" 
+                    value={salaryFrom || ""}
+                    onChange={(e) => setSalaryFrom(e.target.value)} 
+                />
+                <Select
+                    value={type}
+                    onValueChange={(e) => setType(e)}
+                >
+                    <SelectTrigger>
+                        <SelectValue placeholder="Foglalkoztatás típusa" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                        <SelectLabel>Foglalkoztatás típusa</SelectLabel>
+                        <SelectItem value="any">---</SelectItem>
+                        <SelectItem value="full-time">Teljes munkaidő</SelectItem>
+                        <SelectItem value="part-time">Rész munkaidő</SelectItem>
+                        <SelectItem value="contract">Szerződéses</SelectItem>
+                        <SelectItem value="internship">Gyakornok</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <Input 
+                    placeholder="Város" 
+                    value={city || ''} 
+                    onChange={(e) => setCity(e.target.value)} 
+                />
+                <Switch 
+                    value={homeOffice}
+                    onCheckedChange={(e) => setHomeOffice(e)}
+                />
+            </div>
             {error && <div>Error: {JSON.stringify(error)}</div>}
             {isLoading && <div>Loading...</div>}
+            {jobCount} jobs found
             {jobCount > 0 && (
                 <ul>
                     {jobsState.map(job => (
--- client/src/store/reducers/jobApiSlice.js
+++ client/src/store/reducers/jobApiSlice.js
@@ -1,19 +1,31 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const url = "http://localhost:3030/jobs";
+
 export const jobApiSlice = createApi({
     reducerPath: "jobApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3030/jobs",
+        baseUrl: url,
     }),
     endpoints: (build) => ({
-        getJobs: build.query({
-            query: () => ({
-                method: "GET",
-            })
+        getJobsWithFilter: build.query({
+            query: ({ salaryFrom, type, city, homeOffice }) => {
+                const params = {};
+                if (salaryFrom > 0) params["salaryFrom[$gte]"] = salaryFrom;
+                if (type !== undefined && type != "any") params.type = type;
+                if (city !== undefined) params["city"] = city;
+                if (homeOffice) params.homeOffice = homeOffice;
+
+                return {
+                    method: "GET",
+                    params,
+                    url: url
+                };
+            }
         })
     })
 })
 
 export const {
-    useGetJobsQuery
+    useGetJobsWithFilterQuery
 } = jobApiSlice;
